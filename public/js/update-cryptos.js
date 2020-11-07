794952e6ba6ce372dbe1f5d7053fc33628007bfc
will3g <willianpaulo_09@hotmail.com>
2020-11-07 13:03:31 -0300

    update-cryptos

---
+++ public/js/update-cryptos.js
@@ -0,0 +1,20 @@
+setTimeout((() => { 
+    fetch("https://localhost:5001/api/coin/bch");
+    fetch("https://localhost:5001/api/coin/brzx");
+    fetch("https://localhost:5001/api/coin/btc");
+    fetch("https://localhost:5001/api/coin/dai");
+    fetch("https://localhost:5001/api/coin/dash");
+    fetch("https://localhost:5001/api/coin/dcr");
+    fetch("https://localhost:5001/api/coin/eth");
+    fetch("https://localhost:5001/api/coin/ltc");
+    fetch("https://localhost:5001/api/coin/omg");
+    fetch("https://localhost:5001/api/coin/paxg");
+    fetch("https://localhost:5001/api/coin/tusd");
+    fetch("https://localhost:5001/api/coin/usdt");
+    fetch("https://localhost:5001/api/coin/xrp");
+    fetch("https://localhost:5001/api/coin/zec");
+    fetch("https://localhost:5001/api/coin/zrx");
+}), 300000);
+
+// 60000); // 1 minute
+// 300000); // 5 minutes
