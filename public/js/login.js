98f1fe4ebc3c38a268adc58b3ff0462dab27a8cb
will3g <willianpaulo_09@hotmail.com>
2020-11-12 21:33:17 -0300

    connect-login-page-with-api

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,17 @@ const inputCpf = container.querySelector('[name=cpf]');
 const inputPassword = container.querySelector('[name=password]');
 const button = container.querySelector('[data-name=enter]');
 
+function clearVariables() {
+    localStorage.removeItem('name');
+    localStorage.removeItem('cpf');
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+}
+
+function setVariables() {
+    localStorage.setItem('cpf', '');
+}
+
 function Message(txtMessage) {
     let message = container.querySelector('[data-name=message]');
     message.textContent = txtMessage;
@@ -25,6 +36,8 @@ function ValidatePassword(pass) {
 }
 
 (() => {
+    clearVariables();
+    setVariables();
     button.addEventListener('click', function () {
         if (!validateCPF(inputCpf.value))
             Message('CPF preenchido incorretamente.');
@@ -33,15 +46,12 @@ function ValidatePassword(pass) {
             Message('Senha deve ter no mínimo 8 caracteres.');
 
         if (validateCPF(inputCpf.value) && ValidatePassword(inputPassword.value))
-            fetch(`https://localhost:5001/api/user/${inputCpf.value}`).then(res => res.json()).then(response => {
+            fetch(`https://localhost:5001/api/user/login/${inputCpf.value}/${inputPassword.value}`).then(res => res.json()).then(response => {
                 if (response.length > 0) {
-                    if (response[0].senha == inputPassword.value) {
-                        window.location.replace('./dashboard.html');      
-                    } else {
-                        Message('Senha incorreta.');
-                    }
+                    localStorage.setItem('cpf', inputCpf.value);
+                    window.location.replace('./dashboard.html');
                 } else {
-                    Message('Usuário inexistente.');
+                    Message('Usuário ou senha inválida.');
                 }
             });
     });
