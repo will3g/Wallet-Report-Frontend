f8abeefb298d9e707b2bb49a24dd543f0df4930f
will3g <willianpaulo_09@hotmail.com>
2020-11-19 23:48:04 -0300

    fix-layout-and-profile

--- public/js/dashboard/profile.js
+++ public/js/dashboard/profile.js
@@ -30,7 +30,7 @@ function Message(txtMessage = '') {
 	message.classList.add('message-error');
 }
 
-function validationName(name) {
+function validateName(name) {
 	if (/^[a-zA-Z ]{10,40}$/.test(name)) {
 		Message();
 		return name;
@@ -65,11 +65,9 @@ function checkInput(input) {
 	const element = document.querySelector(`#${input}`);
 
 	if (input == 'name')
-		return element.value ? validationName(element.value) : localStorage.getItem(`${input}`);
+		return element.value ? validateName(element.value) : localStorage.getItem(`${input}`);
 	else if (input == 'email')
 		return element.value ? ValidateEmail(element.value) : localStorage.getItem(`${input}`);
-
-	return element.value ? ValidatePassword(element.value) : localStorage.getItem(`${input}`);
 }
 
 function profileEmail(email) {
@@ -115,30 +113,57 @@ function MarkupProfileUser(name, email, cpf) {
 
 button.addEventListener('click', e => {
 	e.preventDefault();
-	const name = checkInput('name');
-	const email = checkInput('email');
-	const password = checkInput('password');
 
-	(async () => {
-		const response = await fetch(`https://localhost:5001/api/user/${cpf}`, {
-			method: 'PUT',
-			mode: 'cors',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				cpf: cpf,
-				nome: name,
-				senha: password,
-				email: email,
-				admin: convertStringToBoolean(admin)
-			})
-		});
+	const name = document.querySelector('#name').value;
+	const email = document.querySelector('#email').value;
 
-		if (response.status == 200) {
-			alert('Cadastro atualizado com sucesso!');
-			window.location.replace('./profile.html');
+	const pass = document.querySelector('#password').value;
+	const rpassword = document.querySelector('#rpassword').value;
+
+	if ((rpassword) && ((ValidatePassword(pass)) == rpassword)) {	
+		if (validateName(name) && ValidateEmail(email)) {
+			(async () => {
+				const response = await fetch(`https://localhost:5001/api/user/${cpf}`, {
+					method: 'PUT',
+					mode: 'cors',
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						cpf: cpf,
+						nome: name ? name : localStorage.getItem('name'),
+						senha: pass,
+						email: email ? email : localStorage.getItem('email'),
+						admin: convertStringToBoolean(admin)
+					})
+				});
+		
+				if (response.status == 200) {
+					alert('Cadastro atualizado com sucesso!');
+					window.location.replace('./profile.html');
+				}
+			})();
 		}
-	})();
+	} else {
+		if (!pass) {
+			if (!email && !name) {
+				Message('Para atualizar seus dados é todos os campos, digitar a senha e confirmar.');
+			} else if (name) {
+				if (validateName(name)) {
+					Message('É necessário prencher todos os campos, digitar a senha e confirmar.');
+				}
+			}
+			else if (email) {
+				if (ValidateEmail(email)) {
+					Message('É necessário prencher todos os campos, digitar a senha e confirmar.');
+				}
+			}
+		} else {
+			if (ValidatePassword(pass)) {
+				Message('Senha não confere.');
+			}
+		}
+	}
+
 });
